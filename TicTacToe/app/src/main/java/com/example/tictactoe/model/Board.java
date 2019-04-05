0d888f76d273a84bd0f4d47dce30d378ce1108b7
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-04-05 17:56:16 -0500

    Added winner check

--- TicTacToe/app/src/main/java/com/example/tictactoe/model/Board.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/model/Board.java
@@ -2,6 +2,8 @@ package com.example.tictactoe.model;
 
 public class Board {
     private char[] board = new char[9];
+    private char mark = 'X';
+    private String markColor = "#0000FF";
 
     public Board() {
         for (int i = 0; i < 9; i++) {
@@ -14,6 +16,43 @@ public class Board {
     }
 
     public void setBoardSpot(int spot, char mark) {
+        this.setMarkAndColor();
         this.board[spot] = mark;
     }
+
+    public char getBoardSpot(int spot) {
+        return this.board[spot];
+    }
+
+    public char getMark() {
+        return mark;
+    }
+
+    public String getMarkColor() {
+        return markColor;
+    }
+
+    public void setMarkAndColor() {
+        if (this.mark == 'X') {
+            this.mark = 'O';
+            this.markColor = "#FF0000";
+        }
+        else {
+            this.mark = 'X';
+            this.markColor = "#0000FF";
+        }
+    }
+
+    public void checkWinner() {
+        if (spotsEqual(0, 1, 2) || spotsEqual(3, 4, 5) || spotsEqual(6, 7, 8) ||
+                spotsEqual(0, 3, 6) || spotsEqual(1, 4, 7) || spotsEqual(2, 5, 8) ||
+                spotsEqual(0, 4, 8) || spotsEqual(2, 4, 6)) {
+            System.out.println("WINNER!");
+        }
+    }
+
+    private boolean spotsEqual(int spot1, int spot2, int spot3) {
+        return board[spot1] != 'E' && board[spot1] == board[spot2] && board[spot2] == board[spot3];
+    }
+
 }
--- TicTacToe/app/src/main/java/com/example/tictactoe/ui/GameActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/ui/GameActivity.java
@@ -18,7 +18,7 @@ public class GameActivity extends AppCompatActivity {
     private TextView player1Name;
 
     private Board board = new Board();
-    private Button button0, button1, button2, button3;
+    private Button button0, button1, button2, button3, button4, button5, button6, button7, button8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +28,14 @@ public class GameActivity extends AppCompatActivity {
         player1Name = findViewById(R.id.player1Name);
 
         button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
 
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
@@ -37,14 +45,73 @@ public class GameActivity extends AppCompatActivity {
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                char mark = 'X';
-                button0.setBackgroundColor(Color.parseColor("#0000FF"));
-                board.setBoardSpot(0, mark);
-                printBoard();
+                placeMarkOnBoard(v, 0);
             }
         });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 1);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 3);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 4);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 5);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 6);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 7);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeMarkOnBoard(v, 8);
+            }
+        });
+    }
+
+    private void placeMarkOnBoard(View v, int i) {
+        char mark = board.getMark();
+        System.out.println("Clicked Spot");
+        if (board.getBoardSpot(i) == 'E') {
+            System.out.println("Clicked valid spot");
+            board.setBoardSpot(i, mark);
+            v.setBackgroundColor(Color.parseColor(board.getMarkColor()));
+            printBoard();
+            board.checkWinner();
+        }
 
     }
+
+
 
     private void printBoard() {
         System.out.println(board.getBoard());
